d6f9cb01b87d47b874816b255558d179900864a7
jyou <jyou@estsecurity.com>
2024-11-05 22:48:29 +0900

    www 서브도메인 추가

--- deployGitImage.bat
+++ deployGitImage.bat
@@ -1,12 +1,23 @@
 @echo off
 
+REM 정확한 Java 17 경로로 설정
+set JAVA_HOME=C:\Program Files\Java\jdk-17
+set PATH=%JAVA_HOME%\bin;%PATH%
+
 REM .env 파일에서 GITHUB_TOKEN 로드
 for /F "tokens=* delims=" %%x in (.env) do (
     set "%%x"
 )
 
 set GITHUB_USERNAME=youjin92
-set IMAGE_NAME=ghcr.io/%GITHUB_USERNAME%/apitest:v1.0.7
+set IMAGE_NAME=ghcr.io/%GITHUB_USERNAME%/apitest:v1.0.8
+
+echo Running Gradle clean and bootJar tasks...
+call gradlew clean bootJar
+if %errorlevel% neq 0 (
+    echo Gradle build failed.
+    exit /b %errorlevel%
+)
 
 echo Logging in to GitHub Container Registry...
 echo %GITHUB_TOKEN% | docker login ghcr.io -u %GITHUB_USERNAME% --password-stdin
